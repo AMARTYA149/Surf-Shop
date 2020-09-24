bba730c1748fe5291d4f7ec0cf119e14448820ba
Amartya Aishwarya <amartya970914@gmail.com>
2020-09-24 19:43:15 +0530

    Setup passport

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+// const passport
+const User = require('./models/user');
 
+
+//require routes
 const indexRouter = require('./routes/index');
 const posts = require('./routes/posts');
 const reviews = require('./routes/reviews');
@@ -20,6 +25,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure Passport and Sessions
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//Mount routes
 app.use('/', indexRouter);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
