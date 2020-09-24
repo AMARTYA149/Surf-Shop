d58cb50c9faa1f68d79f2e6d153a709c676e3e0d
Amartya Aishwarya <amartya970914@gmail.com>
2020-09-24 10:43:00 +0530

    Add user schema/model

---
+++ models/post.js
@@ -0,0 +1,11 @@
+// Post
+// - title - string
+// - price - string
+// - description - string
+// - images - array of strings
+// - author - object id(ref user)
+// - location - string
+// - lat - number
+// - lng - number
+// - reviews
+
---
+++ models/user.js
@@ -0,0 +1,27 @@
+// User
+// - email - string
+// - password - string
+// - username -  string
+// - profilePic - string
+// - post - array of object ref Post
+// - reviews - array of object ref Review
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: String,
+  // password & username field is inbuilt provided by passport mongoose
+  image: String,
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
