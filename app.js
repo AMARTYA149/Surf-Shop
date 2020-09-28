10002864bccf1c53206074825f8a6d0e5aeff693
Amartya Aishwarya <amartya970914@gmail.com>
2020-09-28 21:32:23 +0530

    mongoDB connection established & test /register POST route

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
-// const passport
 const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
 
 //require routes
@@ -14,6 +15,14 @@ const posts = require('./routes/posts');
 const reviews = require('./routes/reviews');
 
 const app = express();
+
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/surf-shop', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("We're Connected!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
