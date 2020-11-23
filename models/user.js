33337f7b73264a4d71e9072f87515d0d38c66e87
Amartya Aishwarya <amartya970914@gmail.com>
2020-11-23 21:09:16 +0530

    images display sized

--- models/user.js
+++ models/user.js
@@ -6,22 +6,16 @@
 // - post - array of object ref Post
 // - reviews - array of object ref Review
 
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   email: String,
   // password & username field is inbuilt provided by passport mongoose
   image: String,
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Post'
-    }
-  ]
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
